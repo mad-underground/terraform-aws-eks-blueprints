5f1371a73506e343ee41956d4620abdab8c58b07
Jeff Landis <jemala125@gmail.com>
2022-12-15 02:22:07 -0500

    include outputs for addons module

---
+++ wrapper/bin/create-addons-outputs.py
@@ -0,0 +1,40 @@
+import re
+import os
+
+output_name_regex = re.compile('^output[\s]*"(.*)"[\s]*\{$')
+description_regex = re.compile('^description[\s]*=[\s]*(".*")$')
+value_regex = re.compile('^value[\s]*=.*$')
+end_regex = re.compile('^.*\}$')
+
+def main():
+    src_file = 'modules/kubernetes-addons/outputs.tf'
+    dest_file = 'wrapper/addons-outputs.tf'
+    output_name = None
+    description = None
+
+    with open(dest_file, 'w') as write_f:
+        with open(src_file, encoding="utf-8") as read_f:
+            for line in read_f:
+                output_name_variable_match = output_name_regex.match(line.strip())
+                if output_name_variable_match:
+                    output_name = output_name_variable_match.group(1).strip()
+                    continue
+                description_match = description_regex.match(line.strip())
+                if description_match:
+                    description = description_match.group(1).strip()
+                    continue
+                end_match = end_regex.match(line.strip())
+                if end_match:
+                    if output_name is None:
+                        continue
+                    write_f.write(f'output "{output_name}" {{\n')
+                    if description is not None:
+                        write_f.write(f'  description = {description}\n')
+                    write_f.write(f'  value = module.kubernetes_addons.{output_name}\n')
+                    write_f.write('}\n\n')
+                    output_name = None
+                    description = None
+
+
+if __name__ == '__main__':
+    main()

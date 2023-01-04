735694ec47eb3f507ec9b7b23581e224a2f409e6
Jeff Landis <jemala125@gmail.com>
2023-01-04 16:58:48 -0500

    return only the first item in list

--- modules/node-groups/outputs.tf
+++ modules/node-groups/outputs.tf
@@ -7,7 +7,11 @@ output "eks_managed_node_groups" {
 }
 
 output "self_managed_node_groups" {
-  value = module.aws_eks_self_managed_node_groups
+  value = {
+    for k1,v1 in module.aws_eks_self_managed_node_groups: k1 => {
+      for k2,v2 in v1: k2 => try(one(v2), v2, null)
+    }
+  }
 }
 
 output "aws_auth_config_map" {
